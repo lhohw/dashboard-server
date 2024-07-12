88dc19403864e64182653dfde299cea88f029eef
lhohw <lho.hw.an1@gmail.com>
2024-07-12 23:30:25 +0900

    feat: api to renew

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,12 @@
 import Router from "@koa/router";
 import posts from "./posts";
 import system from "./system";
+import renew from "./renew";
 
 const api = new Router();
 
 api.use("/posts", posts.routes());
 api.use("/system", system.routes());
+api.use("/renew", renew.routes());
 
 export default api;
