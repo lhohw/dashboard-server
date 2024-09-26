6b7c41701c13f155a34335e103a68d13b98faf82
lhohw <lho.hw.an1@gmail.com>
2024-09-26 18:24:12 +0900

    [feat]: add category change api

--- src/api/system/system.ctrl.ts
+++ src/api/system/system.ctrl.ts
@@ -1,7 +1,10 @@
 import type { RouterContext } from "@koa/router";
 
 import { z } from "zod";
-import { addColumn as _addColumn } from "lib/manipulate";
+import {
+  addColumn as _addColumn,
+  changeCategory as _changeCategory,
+} from "lib/manipulate";
 import { PGBuiltinsTypes } from "const/definitions";
 
 type SystemState = {};
@@ -38,6 +41,33 @@ export const addColumn = async (ctx: SystemContext) => {
   try {
     const { tableName, columnName, data, type, notNull } = requestBody;
     const res = await _addColumn(tableName, columnName, data, type, notNull);
+    ctx.response.body = res;
+  } catch (e: any) {
+    ctx.throw(500, e);
+  }
+};
+
+export const changeCategory = async (ctx: SystemContext) => {
+  const ObjectType = z.object({
+    from: z.string(),
+    to: z.string(),
+  });
+
+  let requestBody: z.infer<typeof ObjectType>;
+  try {
+    requestBody = ObjectType.pick({
+      from: true,
+      to: true,
+    }).parse(ctx.request.body);
+  } catch (e: any) {
+    ctx.status = 400;
+    ctx.message = e.message;
+    return;
+  }
+
+  try {
+    const { from, to } = requestBody;
+    const res = await _changeCategory(from, to);
     ctx.response.body = res;
   } catch (e: any) {
     ctx.throw(500, e);
